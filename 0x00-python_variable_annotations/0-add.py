335c26c238786d84da8c990eebc2a843d343758e
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-18 02:36:35 +0100

    updated task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -9,8 +9,3 @@ def add(a: float, b: float) -> float:
     Adds two float and returns their float value
     """
     return a + b
-
-
-if __name__ == "__main__":
-    print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
