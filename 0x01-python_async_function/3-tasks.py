7c8fda16f9c03489b8e47d07086f476fbc93a750
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-18 23:15:58 +0100

    solved task 3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Creates a task_wait_random sync function"""
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay):
+    """sync wait random function"""
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
