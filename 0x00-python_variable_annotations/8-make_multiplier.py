09176ae81a52efc5d099efe9de480f757cab444c
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-18 02:53:06 +0100

    solved task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""A module containing a function that returns a function that
+takes an argument and multiplies it to the argument of this function"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """makes a multiplier"""
+    def m(n: float) -> float:
+        """multiplier to be returned"""
+        return n * multiplier
+    return m
