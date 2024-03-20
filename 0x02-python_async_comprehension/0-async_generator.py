71af5faa48e9c780b9d6ff5a2afe335c64e3d320
ShegzBit <feranmiawotubo16@gmail.com>
2024-03-20 01:17:38 +0100

    solved task 0[1]

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -11,13 +11,3 @@ async def async_generator() -> float:
     """An async generator of 10 random values"""
     for i in range(10):
         yield await asyncio.sleep(1, result=random.uniform(0, 10))
-
-
-if __name__ == "__main__":
-    async def print_yielded_values():
-        result = []
-        async for i in async_generator():
-            result.append(i)
-        print(result)
-
-    asyncio.run(print_yielded_values())
